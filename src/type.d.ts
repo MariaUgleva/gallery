990dd4ed73e457a2812a3e0e4a12479e3aa0d0bd
MARIYA UGLEVA <79036926086@yandex.ru>
2020-11-19 15:01:15 +0300

    last

--- src/type.d.ts
+++ src/type.d.ts
@@ -3,16 +3,19 @@ export type PictureType = {
 	id: number;
 	url: string;
 };
+// тип подробной информации с сервера для картинки
 export type ModalPictureType = {
 	id: number;
 	url: string;
 	comments?: Array<Comment>;
 };
+// тип комментария
 export type Comment = {
 	id: number;
 	date: number;
 	text: string;
 };
+// тип комментария для отправки на сервер
 export type CommentFetch = {
 	id: number;
 	name: string;
